b41848d0f27e67e998e10aa627a189c6206501fc
leoaugustov <vieira.augustoleonardo@gmail.com>
2021-01-18 19:00:10 -0300

    First commit - setting up project

---
+++ src/main/java/desafiotecnicohotmart/DesafioTecnicoHotmartApplication.java
@@ -0,0 +1,13 @@
+package desafiotecnicohotmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTecnicoHotmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTecnicoHotmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/desafiotecnicohotmart/DesafioTecnicoHotmartApplicationTests.java
@@ -0,0 +1,13 @@
+package desafiotecnicohotmart;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DesafioTecnicoHotmartApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
