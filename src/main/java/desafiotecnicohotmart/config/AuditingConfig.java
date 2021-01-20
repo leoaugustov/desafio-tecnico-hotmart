19bdb64b9eaa9dc04fd5ad183d7c41cf34e86061
leoaugustov <vieira.augustoleonardo@gmail.com>
2021-01-19 23:09:04 -0300

    Auditing successful requests

---
+++ src/main/java/desafiotecnicohotmart/config/AuditingConfig.java
@@ -0,0 +1,20 @@
+package desafiotecnicohotmart.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class AuditingConfig {
+
+	@Bean
+	public CommonsRequestLoggingFilter requestLoggingFilter() {
+	    CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+	    loggingFilter.setIncludeClientInfo(true);
+	    loggingFilter.setIncludeQueryString(true);
+	    loggingFilter.setIncludePayload(true);
+	    loggingFilter.setMaxPayloadLength(5000);
+	    return loggingFilter;
+	}
+	
+}
