4d17bca00a86ef782dc4592aa607a2a20c7a3c0d
leoaugustov <vieira.augustoleonardo@gmail.com>
2021-01-20 23:02:36 -0300

    Entity ProductScore

---
+++ src/main/java/desafiotecnicohotmart/model/ProductScore.java
@@ -0,0 +1,62 @@
+package desafiotecnicohotmart.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Formula;
+import org.hibernate.annotations.GenericGenerator;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "product_score")
+@Getter
+@Setter
+@NoArgsConstructor
+public class ProductScore {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+	@GenericGenerator(name = "native", strategy = "native")
+	@JsonProperty(access = Access.READ_ONLY)
+	private Long id;
+	
+	@CreationTimestamp
+	@Column(nullable = false, updatable = false)
+	@Setter(AccessLevel.NONE)
+	private LocalDate referenceDate;
+	
+	@Column(nullable = false, updatable = false)
+	private double x;
+	
+	@Column(nullable = false, updatable = false)
+	private double y;
+	
+	@Column(nullable = false)
+	private int z;
+	
+	@Formula("x + y + z")
+	@Setter(AccessLevel.NONE)
+	private int score;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "product_id", nullable = false, updatable = false)
+	private Product product;
+	
+}
---
+++ src/main/java/desafiotecnicohotmart/repository/ProductScoreRepository.java
@@ -0,0 +1,9 @@
+package desafiotecnicohotmart.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import desafiotecnicohotmart.model.ProductScore;
+
+public interface ProductScoreRepository extends JpaRepository<ProductScore, Long> {
+
+}
